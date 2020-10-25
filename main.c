173c28275ecf33ba4d53be8866e8452f2f4b4f50
alagarbey <ala.garbey@esprit.tn>
2020-10-25 20:42:20 +0100

    ajout

---
+++ main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"fonctions.h"
+int main()
+{
+int a;
+int b;
+printf("\nSaisie du premiere Nombre:");
+scanf("%d",&a);
+do {
+printf("\nSaisie du Second Nombre:");
+scanf("%d",&b);
+}while(b==0);
+printf("\n%d+%d=%d",a,b,sommer(a,b));
+printf("\n%d-%d=%d",a,b,soustraire(a,b));
+printf("\n%d+%d=%f\n",a,b,diviser(a,b));
+return (0);
+}
+
+
